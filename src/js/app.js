d481672ad8bd6abd357a01e5a393ec9687884f31
Elad Rapaport <nado123@gmail.com>
2018-11-11 21:15:27 +0200

    created array for table and created html table. still need to add tests and add table headers

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,16 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
 import {parseFunction} from './code-parser';
+import {activateStart} from './code-parser';
+import {createTable} from './code-parser';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
-        let test = parseFunction(codeToParse)
-        $('#testlabel').innerText=test;
+        activateStart();
+        let tableArr = parseFunction(codeToParse)
+        createTable();
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
     });
 });
--- src/js/code-parser.js
+++ src/js/code-parser.js
@@ -1,4 +1,5 @@
 import {parseCode} from './code-analyzer';
+import * as escodegen from 'escodegen';
 
 class Row{
     constructor(line, type, name, condition, value){
@@ -10,29 +11,104 @@ class Row{
     }
 }
 
+function createTable(){
+    var table = document.createElement('table');
+    for (var i in rows){
+        console.log('blabla');
+        var tr = document.createElement('tr');
 
-var rows = []
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var td4 = document.createElement('td');
+        var td5 = document.createElement('td');
+
+        var text1 = document.createTextNode(rows[i].line);
+        var text2 = document.createTextNode(rows[i].type);
+        var text3 = document.createTextNode(rows[i].name);
+        var text4 = document.createTextNode(rows[i].condition);
+        var text5 = document.createTextNode(rows[i].value);
+
+        td1.appendChild(text1);
+        td2.appendChild(text2);
+        td3.appendChild(text3);
+        td4.appendChild(text4);
+        td5.appendChild(text5);
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+
+        table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+}
+
+var rows = [];
 var start = true;
-function parseFunction(ast) {
+function activateStart(){
+    start = true;
+    rows = [];
+}
+function parseFunction(ast, isElse=false){
     if(start){
         ast = parseCode(ast);
         start = false;
     }
-    console.log(ast.type);
-    rows.push(new Row(ast.type));
-    if(ast.type == 'Program'){
-        for(var row in ast.body[0]){
-            parseFunction(row);
-        }
-        // ast.body.forEach ((row, index) => {
-        //     parseFunction(row);
-        // });
+    let line = ast.loc.start.line;
+    switch(ast.type){
+        case 'FunctionDeclaration':
+            rows.push(new Row(line, 'function declaration', ast.id.name, null, null));
+            for(var param in ast.params)
+                parseFunction(ast.params[param]);
+            break;
+        case 'Identifier':
+            rows.push(new Row(line, 'variable declaration', ast.name, null, null));
+            break;
+        case 'VariableDeclaration':
+            for(var decl in ast.declarations)
+                parseFunction(ast.declarations[decl])
+            break;
+        case 'VariableDeclarator':
+            rows.push(new Row(line, 'variable declaration', ast.id.name, null, null));
+            break;
+        case 'WhileStatement':
+            // rows.push(new Row(line, 'while statement', null, stringifyTest(ast), null));
+            rows.push(new Row(line, 'while statement', null, escodegen.generate(ast.test), null));
+            break;
+        case 'IfStatement':
+            // rows.push(new Row(line, 'if statement', null, ast.test.left.name + ast.test.operator + ast.test.right.name))
+            let ifStr = 'if statement';
+            if(isElse)
+                ifStr = 'else if statement';
+            rows.push(new Row(line, ifStr, null, escodegen.generate(ast.test), null));
+            parseFunction(ast.consequent);
+            if(ast.hasOwnProperty('alternate')){
+                parseFunction(ast.alternate, isElse=true);
+            }
+            break;
+        case 'ExpressionStatement':
+            if(ast.expression.type == 'AssignmentExpression')
+                rows.push(new Row(line, 'assignment expression', ast.expression.left.name, null, escodegen.generate(ast.expression.right)));
+            break;
+        case 'ReturnStatement':
+            rows.push(new Row(line, 'return statement', null, null, escodegen.generate(ast.argument)));
+            break;
     }
-
-
-    // if(stop < 2)
-    //     parseFunction(ast.body);
-    // console.log(ast.body)
+    if(ast.hasOwnProperty('body')){
+        if(ast.body.constructor === Array){
+            for(var row in ast.body){
+                parseFunction(ast.body[row]);
+            }
+        }
+        else{
+            parseFunction(ast.body);
+        }
+    }
 }
 
 export {parseFunction};
+export {activateStart};
+export {createTable};
